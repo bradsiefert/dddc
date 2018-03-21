d1316995af5cb0759ca755f9a262acacd3094aa9
Brad <bradsiefert@icloud.com>
2018-03-20 21:49:30 -0500

    Working Tuesday night
    Finished the top block and most of the event block

--- index.php
+++ index.php
@@ -2,10 +2,66 @@
 
 <div class="container hero">
     <div class="row justify-content-center">
-        <div class="col-xl-8">
+        <div class="offset-lg-1 col-lg-10 col-xl-8">
             <div class="top-slogan">
-                <span class="primary">Democrati<span class="flag-follow"><img class="flag" src="/wp-content/themes/dddc-theme/images/icon-flag.svg" alt="Flag Icon">c</span>
-                </span> Developers & Designers <em>of Chicago</em>
+                <h1>
+                    <span class="primary">Democrati<span class="flag-follow"><img class="flag" src="/wp-content/themes/dddc-theme/images/icon-flag.svg" alt="Flag Icon">c</span></span> Developers & Designers <em>of Chicago</em>
+                </h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed sapien quam. Sed dapibus est
+                    id enim facilisis, at posuere turpis adipiscing.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="event">
+    <div class="container">
+        <div class="row">
+            <div class="offset-lg-1 col-lg-12">
+                <h2>Our Next Event</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="offset-lg-1 col-lg-3 order-lg-1 order-1">
+                <dl>
+                    <dt>Time & Date</dt>
+                    <dd>Tuesday, April 23rd, 12pm</dd>
+                </dl>
+
+                <dl>
+                    <dt>Location</dt>
+                    <dd>
+                        <address>
+                            Earlybird Software <br>
+                            218 South Wabash Avenue #280 <br>
+                            Chicago, IL 60604
+                        </address>
+                    </dd>
+                </dl>
+            </div>
+            <div class="col-lg-4 order-lg-2 order-3">
+                <dl>
+                    <dt>Details</dt>
+                    <dd>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed sapien quam. Sed dapibus est
+                        id enim facilisis, at posuere. Nulla nec varius dui. Aliquam erat volutpat.
+                    </dd>
+                    <div class="btn btn-danger">Sign Up To Attend</div>
+                </dl>
+            </div>
+            <div class="col-lg-4 order-lg-3 order-2">
+                <dl>
+                    <dt>Speaker</dt>
+                    <div class="media">
+                        <img class="align-self-center rounded-circle mr-3" src="/wp-content/themes/dddc-theme/images/speaker-frerichs.jpg" alt="Generic placeholder image">
+                        <div class="media-body">
+                            <h4 class="mb-1">Michael Frerichs</h5>
+                            <p>State of Illinois Treasurer</p>
+                        </div>
+                    </div>
+                </dl>
             </div>
         </div>
     </div>
